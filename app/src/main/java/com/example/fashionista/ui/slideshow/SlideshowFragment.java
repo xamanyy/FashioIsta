025d9e70fac44835cd18f29f3e899b828d884fef
Anmol <anmolkamboj@gmail.com>
2021-11-16 23:29:41 +0530

    Etc

--- app/src/main/java/com/example/fashionista/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/fashionista/ui/slideshow/SlideshowFragment.java
@@ -28,7 +28,7 @@ public class SlideshowFragment extends Fragment {
         binding = FragmentSlideshowBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSlideshow;
+        final TextView textView = binding.textView2;
         slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
